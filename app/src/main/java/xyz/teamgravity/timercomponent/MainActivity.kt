1f8b7d862eb44f2131196437ad491dc9efa00132
Raheem <raheemideadev@gmail.com>
2021-09-28 20:08:06 +0500

    timer

--- app/src/main/java/xyz/teamgravity/timercomponent/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/timercomponent/MainActivity.kt
@@ -3,18 +3,33 @@ package xyz.teamgravity.timercomponent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import xyz.teamgravity.timercomponent.ui.theme.TimerComponentTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Surface(
+                color = Color(0xFF101010),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(contentAlignment = Alignment.Center) {
+                    Timer(
+                        totalTime = 8L * 1000L,
+                        handleColor = Color.Green,
+                        inactiveBarColor = Color.DarkGray,
+                        activeBarColor = Color.Green,
+                        modifier = Modifier.size(200.dp)
+                    )
+                }
+            }
         }
     }
 }
