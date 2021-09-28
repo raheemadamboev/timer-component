beb219e27bd518e1edb935dbcb9c16208b58fab2
Raheem <raheemideadev@gmail.com>
2021-09-28 19:11:47 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/timercomponent/MainActivity.kt
@@ -0,0 +1,20 @@
+package xyz.teamgravity.timercomponent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.timercomponent.ui.theme.TimerComponentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
